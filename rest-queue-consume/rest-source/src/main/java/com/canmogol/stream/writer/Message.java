dd2929935110bb19d130d6b9a5c28ce23e1048aa
Can MOGOL <canmogol@gmail.com>
2021-12-21 18:45:42 +0300

    Add files via upload

---
+++ rest-queue-consume/rest-source/src/main/java/com/canmogol/stream/writer/Message.java
@@ -0,0 +1,12 @@
+package com.canmogol.stream.writer;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Message {
+    private String content;
+}
---
+++ source-processor-sink/source/src/main/java/com/canmogol/stream/source/Alert.java
@@ -0,0 +1,12 @@
+package com.canmogol.stream.source;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Alert {
+    private String content;
+}
